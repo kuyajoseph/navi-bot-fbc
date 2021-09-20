31127757f95193d98967e4cd2ebc6540fd8df841
JosephARamos <jasramos004@gmail.com>
2021-09-20 16:33:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({intents: 32767})
+const { Client, Intents } = require('discord.js');
+
+const myIntents = new Intents();
+myIntents.add(Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS);
+
+const client = new Client({ intents: myIntents });
 
 const prefix = '+';
 
