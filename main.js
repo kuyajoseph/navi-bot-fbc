980f352ce9e837616c05c67963ca0e27322f153c
JosephARamos <jasramos004@gmail.com>
2021-09-20 20:29:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -31,4 +31,4 @@ client.on('messageCreate', message =>{
 })
 require('./server')();
 client.login(process.env.TOKEN);
-const mySecret = process.env['TOKEN']
+const mySecret = process.env['NzUzMDU0ODg1MTc5NTU1OTgw.X1gnAw.nh15YF9oQe9Y85YjsX4qJR59-k4']
