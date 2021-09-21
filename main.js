38ca307007b67db5a23a6d64704fc3b9a79dca85
JosephARamos <jasramos004@gmail.com>
2021-09-20 18:01:05 -0700

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,7 @@ client.on('guildMemberAdd', guildMember =>{
 
 client.on('messageCreate', message =>{
 
-})
     if(!message.content.startsWith(prefix) || message.author.bot) return;
-
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
@@ -28,7 +26,8 @@ client.on('messageCreate', message =>{
         message.channel.send("Supports on the Way! https://www.youtube.com/codelyon");
     } else if (command === 'welcome'){
         message.channel.send("Welcome <@753054885179555980> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. We pray you're blessed, prepared for edification, and ready to have a fun, Christ-centered fellowship with us! We hope you enjoy your stay.");
-    } 
+    }
+})
 
 require('./server')();
 client.login(process.env.TOKEN);
