6fee9f948d9ae0780d7d1481646dbc40e3f29362
JosephARamos <jasramos004@gmail.com>
2021-09-23 23:33:42 -0700

    updates

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const client = new Discord.Client({intents: 32767})
 
 const prefix = process.env.PREFIX;
+const privateMessage = require('./private-message')
 
 client.once('ready', () => {
     console.log('Systems online!');
@@ -27,6 +28,9 @@ client.on('messageCreate', message =>{
         message.channel.send("Welcome <@753054885179555980> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. We pray you're blessed, prepared for edification, and ready to have a fun, Christ-centered fellowship with us! We hope you enjoy your stay.");
     }
 
+    client.users.fetch('347410931476725781').then((user) => {
+        user.send('small pp')
+    }) 
 })
 require('./server')();
 client.login(process.env.TOKEN);
