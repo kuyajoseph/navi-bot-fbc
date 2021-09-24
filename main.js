a1fd21c9a75ccb897c96c046149d667ea203b1bc
JosephARamos <jasramos004@gmail.com>
2021-09-24 12:26:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ client.on('messageCreate', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'help'){
+    if (command === 'help'){
         message.channel.send("Support's on the Way! https://www.youtube.com/codelyon");
     } else if (command === 'welcome'){
         message.channel.send("Welcome <@753054885179555980> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. We pray you're blessed, prepared for edification, and ready to have a fun, Christ-centered fellowship with us! We hope you enjoy your stay.");
