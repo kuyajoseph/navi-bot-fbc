2db76f3c0a828f11a29da4c2cf42f2b409304e4c
JosephARamos <jasramos004@gmail.com>
2021-09-24 01:16:32 -0700

    update

--- main.js
+++ main.js
@@ -26,9 +26,12 @@ client.on('messageCreate', message =>{
     } else if (command === 'welcome'){
         message.channel.send("Welcome <@753054885179555980> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. We pray you're blessed, prepared for edification, and ready to have a fun, Christ-centered fellowship with us! We hope you enjoy your stay.");
     } else if (command === 'dm'){
-        message.author.send("Hello World!");
-    }
-
+        message.author.send("Ain't no time like the present, whadaya need?");
+    } else if (command === 'users')
+        client.users.fetch('389558217035874308').then(user => {
+        user.send("Hello World!");
+    })
 })
+
 require('./server')();
 client.login(process.env.TOKEN);
--- server.js
+++ server.js
@@ -25,9 +25,17 @@ client.on('messageCreate', message =>{
   const command = args.shift().toLowerCase();
 
   if(command === 'help'){
-      message.channel.send("Support's on the Way! https://www.youtube.com/codelyon");
-  }
-});
+    message.channel.send("Support's on the Way! https://www.youtube.com/codelyon");
+} else if (command === 'welcome'){
+    message.channel.send("Welcome <@753054885179555980> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. We pray you're blessed, prepared for edification, and ready to have a fun, Christ-centered fellowship with us! We hope you enjoy your stay.");
+} else if (command === 'dm'){
+    message.author.send("Ain't no time like the present, whadaya need?");
+} else if (command === 'users')
+    client.users.fetch('389558217035874308').then(user => {
+    user.send("Hello World!");
+})
+})
+
 // You really don't want your token here since your repl's code
 // is publically available. We'll take advantage of a Repl.it 
 // feature to hide the token we got earlier. 
