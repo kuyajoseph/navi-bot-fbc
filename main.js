1b411c7d3dbff90f885efbcca1aabaf4818d0eea
JosephARamos <jasramos004@gmail.com>
2022-07-26 17:59:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,9 @@ client.on('messageCreate', message =>{
 
 })
 
-    if (command === 'welcome'){
+    if (command === 'help'){
+    message.channel.send("Support's on the Way! https://www.youtube.com/codelyon");
+    } else if (command === 'welcome'){
         message.channel.send("Welcome <@441379280102883329> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. Are you ready to steal the world's treasure and take their hearts? I believe that your life will change, so wake up, get up, and get out there. It's showtime!");
     } else if (command === 'dm'){
         message.author.send("Ain't no time like the present, whadaya need?");
