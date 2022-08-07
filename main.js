9e4101db9bc650cd6c8e87ad19f1cb6b75072c5b
Joseph A Ramos <jasramos004@gmail.com>
2022-08-06 17:13:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ client.on('guildMemberAdd', guildMember =>{
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'everyone');
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('752955435681841252').send(`Welcome <@${guildMember.user.id}> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. Are you ready to steal the world's treasure and take their hearts? I believe that your life will change, so wake up, get up, and get out there. It's showtime!`)
-});
 
 client.on("guildMemberUpdate", (oldMember, newMember) => {
     const oldStatus = oldMember.premiumSince;
@@ -154,6 +153,7 @@ client.on('messageCreate', message => {
 });
 });
 });
-})
+});
+});
 require('./server')();
 client.login(process.env.TOKEN);
