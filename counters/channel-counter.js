06d0f067ca52146423fa7cf4c40ebd3970966f91
Joseph A Ramos <jasramos004@gmail.com>
2022-08-19 23:03:19 -0700

    Delete counters directory

--- counters/channel-counter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) =>{
-    const guild = client.guilds.cache.get('752951481937690765');
-    setInterval(() =>{
-        const channelCount = guild.channelCount;
-        const channel = guild.channels.cache.get('1010412675316068453')
-        channel.setName(`Channel Count: ${channelCount.toLocaleString()}`)
-        console.log('Updating Channel Count');
-    }, 5000);
-}
--- counters/user-counter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) =>{
-    const guild = client.guilds.cache.get('752951481937690765');
-    setInterval(() =>{
-        const userCount = guild.userCount;
-        const channel = guild.channels.cache.get('11010411364973551617')
-        channel.setName(`User Count: ${userCount.toLocaleString()}`)
-        console.log('Updating User Count');
-    }, 5000);
-}
