dc69b3a4d352dbad14c74cb283e01edf15834327
Joseph A Ramos <jasramos004@gmail.com>
2022-09-02 23:04:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ client.on('messageCreate', message => {
     if (command === 'help'){
         message.channel.send("Support's on the Way! https://www.youtube.com/codelyon , https://sourceb.in/ , https://dashboard.heroku.com/apps , and https://uptimerobot.com/login?rt=https://uptimerobot.com/dashboard#789227729");
     } else if (command === 'welcome'){
-        message.channel.send("Welcome <@793143330455420938> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. Are you ready to steal the world's treasure and take their hearts? I believe that your life will change, so wake up, get up, and get out there. It's showtime!");
+        message.channel.send("Welcome <@1000977103942078565> to the official FBC Youth server! Please read our <#752955457647149089> and <#752955583749161033>, then introduce yourself in <#752955636043743256>. Are you ready to steal the world's treasure and take their hearts? I believe that your life will change, so wake up, get up, and get out there. It's showtime!");
     } else if (command === 'dm'){
         message.author.send("Ain't no time like the present, whadaya need?");
     } else if (command === 'boost'){
@@ -127,9 +127,12 @@ client.on('messageCreate', message => {
         user.send("Heya Miguel! I'm currently doing the weekly updates for the youth server and noticed you haven't reacted to the 'Vallejo', 'Community Events', 'Matchmaking', and 'Metagame' roles, meaning you don't have access to the entire server. I don't want you to miss out some stuff, so in order to change that, please head on over and read the info in the 'rules' and 'flairing' channels, then self-assign yourself by reacting to the roles you're missing. (consider it a free buff!) See ya on the next mission!");
         client.users.fetch('379018505149874186').then(user => {
         user.send("Heya Bodek! I'm currently doing the weekly updates for the youth server and noticed you haven't reacted to the 'San Francisco', 'Community Events', 'Matchmaking', and 'Metagame' roles, meaning you don't have access to the entire server. I don't want you to miss out some stuff, so in order to change that, please head on over and read the info in the 'rules' and 'flairing' channels, then self-assign yourself by reacting to the roles you're missing. (consider it a free buff!) See ya on the next mission!");
+        client.users.fetch('1000977103942078565').then(user => {
+        user.send("Heya Jaylen! I'm currently doing the weekly updates for the youth server and noticed you haven't reacted to the 'Visitor', 'Community Events', 'Matchmaking', and 'Metagame' roles, meaning you don't have access to the entire server. I don't want you to miss out some stuff, so in order to change that, please head on over and read the info in the 'rules' and 'flairing' channels, then self-assign yourself by reacting to the roles you're missing. (consider it a free buff!) See ya on the next mission!");
         });
     });
     });
+});
 });
 });
 });
