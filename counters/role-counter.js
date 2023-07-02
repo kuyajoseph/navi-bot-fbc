4afeec469f1d09d69704de5e8afccdd56595e543
Joseph Ramos <jasramos004@gmail.com>
2023-07-02 12:39:10 -0700

    Update main.js

--- counters/role-counter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) =>{
-    const guild = client.guilds.cache.get('752951481937690765');
-    setInterval(() =>{
-        const roleCount = guild.roleCount;
-        const channel = guild.channels.cache.get('1010412623084396546')
-        channel.setName(`Role Count: ${memberRole.toLocaleString()}`)
-        console.log('Updating Role Count');
-    }, 5000);
-}
